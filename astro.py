62d34d6f656a5f8fd963af5e188beccb379fbdfc
g.goessel <g_goessel@outlook.com>
2013-10-04 19:33:53 +0200

    cleaned hearder and fixed indent

--- astro.py
+++ astro.py
@@ -1,15 +1,16 @@
-#!/usr/bin/env python3.3
 # -*- coding: utf-8 -*-
+
 date = input('entre ta date de naissance (jj/mm)\n')
 while 1:
     try:
         date = date.split("/")
+        #Si la conversion à échouée date est toujours une string
         if isinstance(date, str):
             raise
         jj = int(date[0])
         mm = int(date[1])
         break
     except:
-            print('try again')
-            date = input('entre ta date de naissance\n')
+        print('try again')
+        date = input('entre ta date de naissance\n')
 print('jour :', jj, ' \n mois :', mm)
