005ff3da0ad994123efd85c501e923070cd516fc
g.goessel <g_goessel@outlook.com>
2013-10-04 19:18:50 +0200

    first commit

---
+++ 1.py
@@ -0,0 +1,5 @@
+year = int(input("année? \n"))
+if year % 4 == 0 or (year % 100 == 0 and year % 400 == 0):
+        print('et ouais c\'est bissextile !\nC\'est qui le papa  ?!?!?')
+else:
+    print("This is no bissextil mother fucker !")
---
+++ astro.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3.3
+# -*- coding: utf-8 -*-
+date = input('entre ta date de naissance (jj/mm)\n')
+while 1:
+    try:
+        date = date.split("/")
+        if isinstance(date, str):
+            raise
+        jj = int(date[0])
+        mm = int(date[1])
+        break
+    except:
+            print('try again')
+            date = input('entre ta date de naissance\n')
+print('jour :', jj, ' \n mois :', mm)
