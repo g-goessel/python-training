6fbf77c88854ab11840b7e4dc1df0fa946085ab1
g.goessel <g_goessel@outlook.com>
2013-10-12 23:37:43 +0200

    now check if capital letter + skips others

--- next_char.py
+++ next_char.py
@@ -3,14 +3,24 @@
 # je déclare les variables
 sortie=''
 
-lettres='abcdefghijklmnopqrstuvwxyz'
+lettres_min='abcdefghijklmnopqrstuvwxyz'
+lettres_max=lettres_min.upper()
 e='éè'
 entree=input('phrase ? \n')
 for i in entree:
-    search=lettres.find(i)
-    if search!=-1:
-        sortie+=lettres[search+1]
+    if lettres_min.find(i)!=-1:
+        if lettres_min.find(i)!=25:
+            sortie+=lettres_min[lettres_min.find(i) + 1]
+        else:
+            sortie+=lettres_min[0]
     elif e.find(i)!=-1:
         sortie+='f'
+    elif lettres_max.find(i)!=-1:
+        if lettres_max.find(i)!=25:
+            sortie+=lettres_max[lettres_max.find(i) + 1]
+        else:
+            sortie+=lettres_max[0]
+    else:
+        sortie+=i
 
 print(sortie)
