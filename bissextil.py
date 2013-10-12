fd8383a12642a51cb414f96c4f416628b873d972
g.goessel <g_goessel@outlook.com>
2013-10-12 23:22:33 +0200

    fixed the prog : now it works fine!
    Also now it recognizes well 'e' dérivates

--- bissextil.py
+++ bissextil.py
@@ -1,5 +1,5 @@
 year = int(input("annee? \n"))
 if (year % 4 == 0 and year %100 !=0) or (year % 400 == 0):
-        print('et ouais c\'est bissextile !\nC\'est qui le papa  ?!?!?')
+    print('et ouais c\'est bissextile !\nC\'est qui le papa  ?!?!?')
 else:
     print("This is no bissextil you mother fucker !")
--- next_char.py
+++ next_char.py
@@ -1,18 +1,16 @@
 # -*- coding: utf-8 -*-
 
+# je déclare les variables
+sortie=''
 
-lettres=['abcdefghijklmnopqrstuvwxyz']
-e=['é','è']
+lettres='abcdefghijklmnopqrstuvwxyz'
+e='éè'
 entree=input('phrase ? \n').lower()
-sortie_list=[]
-sortie_str=''
-for i in range (len(entree)):
-    if entree[i] in lettres:
-        sortie_list.append(entree[i])
-    elif entree[i] in e:
-        sortie_list.append('F')
-    else:
-        sortie_list.append(entree[i])
-for i in sortie_list:
-    sortie_str+=i
-print(sortie_str)
+for i in entree:
+    search=lettres.find(i)
+    if search!=-1:
+        sortie+=lettres[search+1]
+    elif e.find(i)!=-1:
+        sortie+='f'
+
+print(sortie)
