a1f26e92bcbbf7ad9dbfd574a097232575dd7ee2
g.goessel <g_goessel@outlook.com>
2013-10-19 17:41:36 +0200

    added many docstrings

---
+++ Yams/yams.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+    Author : GOESSEL Guillaume (g_goessel@outlook.com)
+"""
+
+# jeu du yams
+from yams_func import *
+
+#nombres aléatoires des 6 dés
+alea = []
+
+for i in range(5):
+    alea.append(random.randint(1, 6))
+print(alea)
+
+
+#premiere demande de relance
+nbr_relances = demande('int', 'combien de dés veux tu relancer ?')
+relance(nbr_relances, alea)
+
+
+#deuxieme demande de relance si on n'a pas refusé la remière
+if nbr_relances != 0:
+    print('\nPremière relance effectuée !')
+    print('Vos dés sont maintenant :')
+    print(alea,'\n')
+
+    nbr_relances = demande('int', 'combien de dés veux tu relancer ?')
+    relance(nbr_relances, alea)
+    print('Relances terminées !')
+    print('\nVos dés sont maintenant :')
+    print(alea)
--- Yams/yams_func.py
+++ Yams/yams_func.py
@@ -1,42 +1,53 @@
-# fichier de fonctions
+"""
+    Author : GOESSEL Guillaume (g_goessel@outlook.com)
+
+    Ceci est le fichier ou sont stockée mes fonctions
+"""
 
 import random
 
-#demande permet d'obtenir une variable du meme type que ref
-def demande(type_ref,phrase):
+def demande(type_ref, phrase):
+    """
+        Cette fonction permet de demande à l'utilisateur de rentrer une
+        variable du type de "type_ref" avec comme question "phrase"
+    """
     while 1:
-        input_var=input(phrase)
+        input_var = input(phrase)
         #je vérifie si l'entrée est correcte
         try:
-            if type_ref=='int':
+            if type_ref == 'int':
                 return int(input_var)
-            elif type_ref=='str':
+            elif type_ref == 'str':
                 return str(input_var)
-            elif type_ref=='float':
+            elif type_ref == 'float':
                 return float(input_var)
         except:
             print('erreur de donnée, recommence')
             continue
 
-        if type(nbr_relances_1)==type(ref):
+        if type(phrase) == type(type_ref):
             break
 
 
 
-def relance(nbr,inter):
-    #on effectue les relances souhaitées
-    deja_rejoues=[]
+def relance(nbr, inter):
+    """
+        Cette fonction exécute le nombre demandé de relances (nbr)
+        Le paramètre inter permet de savoir la combinaison initiale
+        On retourne la nouvelle combinaison de dés
+    """
+    deja_rejoues = []
     for i in range(nbr):
         while 1:
-            k=demande('int','Quel dé veux tu relancer ?\n')
+            k = demande('int', 'Quel dé veux tu relancer ?\n')
             if k in deja_rejoues:
                 print('tu ne peux pas relancer ce dé plusieurs fois !')
             elif type(k) is int and k<=5 and k>0:
-                print('dé ',k,' rejoué')
+                print('dé ', k, ' rejoué')
                 deja_rejoues.append(k)
                 break
             else:
                 print('il est impossible de rejouer ce dé car il n\'existe pas !')
-        #on prend l'objet k-1 pour avoir une lecture naturelle de l'indice du dé
-        inter[k-1]=random.randint(1, 6)
+        #on prend l'objet k-1 pour avoir un indice plus naturel
+        inter[k-1] = random.randint(1, 6)
     return inter
