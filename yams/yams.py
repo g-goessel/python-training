a04cd7db68ff96a978a04d7fa1009cc34a991bf8
g-goessel <g_goessel@outlook.com>
2013-10-30 23:27:56 +0100

    You now have 13 tries

--- yams/yams.py
+++ yams/yams.py
@@ -6,48 +6,52 @@
 # jeu du yams
 from yams_func import *
 
-#liste contenant les nombres aléatoires des 5 dés
-alea = []
+#on a 13 essais
+for num_essaie in range(12):
 
-for i in range(5):
-    alea.append(random.randint(1, 6))
-print(alea)
+    #liste contenant les nombres aléatoires des 5 dés
+    alea = []
 
-
-#premiere  relance
-#on appel alea_rel1 la liste contenant la valeur des dés après la première relance
-
-alea_rel1=jette_des(alea)
-
-#deuxieme  relance si la première n'a pas été refusée
-if alea != alea_rel1:
-    alea=jette_des(alea_rel1)
-else:
-    alea=alea_rel1
-    print('Relances terminées !')
-    print('\nVos dés sont maintenant :')
+    for i in range(5):
+        alea.append(random.randint(1, 6))
     print(alea)
 
 
-#calcul des points
-print('Nous allons maintenant calculer vos points')
-while 1:
-    methode_compt=demande(' ','Voulez vous prendre un numéro ou une figure ? (n/f)')
-    #dans le cas ou le joueur veut compter suivant les numéros
-    if methode_compt=='n':
-        while 1:
-            nbr=demande(0,'Quel numéro voulez vous choisir ?')
-            if nbr>=1 and nbr <=6:
-                points=alea.count(nbr)
-                break
-            else:
-                print('Les valeurs possibles d\'un dés sont comprises entre 1 et 6')
+    #premiere  relance
+    #on appel alea_rel1 la liste contenant la valeur des dés après la première relance
+
+    alea_rel1=jette_des(alea)
+
+    #deuxieme  relance si la première n'a pas été refusée
+    if alea != alea_rel1:
+        alea=jette_des(alea_rel1)
+    else:
+        alea=alea_rel1
+        print('Relances terminées !')
+        print('\nVos dés sont maintenant :')
+        print(alea)
+
+
+    #calcul des points
+    print('Nous allons maintenant calculer vos points')
+    while 1:
+        methode_compt=demande(' ','Voulez vous prendre un numéro ou une figure ? (n/f)')
+        #dans le cas ou le joueur veut compter suivant les numéros
+        if methode_compt=='n':
+            while 1:
+                nbr=demande(0,'Quel numéro voulez vous choisir ?')
+                if nbr>=1 and nbr <=6:
+                    points=alea.count(nbr)
+                    break
+                else:
+                    print('Les valeurs possibles d\'un dés sont comprises entre 1 et 6')
+
+        #dans le cas ou le joueur veut compter suivant une figure
+        elif methode_compt=='f':
+            while 1:
+                fig=demande(0,'Quelle figure voulez vous prendre ?')
             break
-    #dans le cas ou le joueur veut compter suivant une figure
-    elif methode_compt=='f':
-        while 1:
-            fig=demande(0,'Quelle figure voulez vous prendre ?')
+        else :
+            print('Erreur de donnée, recommencez')
         break
-    else :
-        print('Erreur de donnée, recommencez')
-print('Vous avez', points,'points')
+    print('Vous avez obtenus', points,'points à l\'essai', num_essaie+1, '\n')
