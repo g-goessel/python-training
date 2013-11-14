8702d336699215e868ff25b6bea8497c0e259dc6
g.goessel <g_goessel@outlook.com>
2013-11-15 00:20:06 +0100

    DS 1 ex 3 is now 4-threaded !

--- Devoirs/DS 1/exercice_3.py
+++ Devoirs/DS 1/exercice_3.py
@@ -7,13 +7,16 @@ Created on Fri Nov  8 13:06:31 2013
 
 from pylab import *
 from random import randint
+import threading
+import time
 
 liste_x=[]
 liste_y=[]
+time_origin=time.clock()
 
 def brown(p):
     y=0
-    for i in range(p):
+    for k in range(p):
        a=randint(0,2)
        if a==1:
            y+=1
@@ -21,13 +24,37 @@ def brown(p):
            y-=1
     return y
 
-for i in range(100000):
+def temp_multi():
+  a=brown(50)
+  if a in liste_x:
+    liste_y[liste_x.index(a)]+=1
+  else:
+    liste_x.append(a)
+    liste_y.append(1)
+
+def Thread_1():
+  for i in range(100000):
     a=brown(50)
     if a in liste_x:
-        liste_y[liste_x.index(a)]+=1
+      liste_y[liste_x.index(a)]+=1
     else:
-        liste_x.append(a)
-        liste_y.append(1)
+      liste_x.append(a)
+      liste_y.append(1)
 
+t1 = threading.Thread(target=Thread_1)
+t2 = threading.Thread(target=Thread_1)
+t3 = threading.Thread(target=Thread_1)
+t4 = threading.Thread(target=Thread_1)
+t1.start()
+t2.start()
+t3.start()
+t4.start()
+t1.join()
+t2.join()
+t3.join()
+t4.join()
+
+time_end=time.clock()
+print(time_end - time_origin)
 plot(liste_x, liste_y,'bo')
 show()
