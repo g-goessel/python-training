33aeb12aadb9a6ab0782f4d06a05448dacf84ca9
g.goessel <g_goessel@outlook.com>
2013-11-14 22:35:48 +0100

    Corrected the last exercice of DS 1

--- Devoirs/DS 1/exercice_3.py
+++ Devoirs/DS 1/exercice_3.py
@@ -13,20 +13,21 @@ liste_y=[]
 
 def brown(p):
     y=0
-    for i in range(p+1):
-       a=randint(0,2) 
+    for i in range(p):
+       a=randint(0,2)
        if a==1:
            y+=1
        else:
            y-=1
     return y
-    
-for i in range(100):
+
+for i in range(100000):
     a=brown(50)
     if a in liste_x:
         liste_y[liste_x.index(a)]+=1
     else:
         liste_x.append(a)
         liste_y.append(1)
-        
+
 plot(liste_x, liste_y,'bo')
+show()
