dcb80bd56259708e4a090e0bac0bfa82fcab2b52
g.goessel <g_goessel@outlook.com>
2014-01-15 01:30:16 +0100

    Reworked Goldbach : multiprocessing and better notification + text output

--- Defis Maths/Goldbach.py
+++ Defis Maths/Goldbach.py
@@ -1,62 +1,66 @@
 from random import randint
 from math import sqrt, log, floor
-from lib.pynma import pynma
 from multiprocessing import Pool
+import sys
 import time
 import gmpy2
 #see here for gmpy2 : http://code.google.com/p/gmpy/
 
-# 50x on calcul 10000 termes en meme temps
-# je ne dépasse pas 10000 pour ne pas saturer la mémoire
-etape = 10**6
-
-#nombre de fois où je calcul les "etape" termes
-#ainsi on calcul au total repet*etape termes
-repet = 50
-
-#n représente le nombre de départ à partir on commence à calculer
-n = 10**18
+#bibliotheques perso, a supprimer :
+from lib.pynma import pynma
+from lib import personal_keys
 
 #API perso pour l'envoi de notifs à mon tel
-p = pynma.PyNMA( "a7dec75df0f46a2525c378538ed931decaeab3fba94ec22c")
+p = pynma.PyNMA(personal_keys.perso("NMA"))
+
+# Désigne le nombre de calculs à effectuer en meme temps
+# Ne pas dépasser 10**6 pour 3Go de RAM libre
+etape = 10**4
+
+# Nombres d'étapes nécessaires avant de recevoir un message d'etat du calcul
+step_msg=10**4
+
+#Adresse du fichier où sont stockés les résultats
+out_dir='/mnt/sdc1/Goldbach.txt'
+#On enlève ce qu'il y avait dedans
+open(out_dir,'w').close()
+
+
 
 def goldbach(n):
     """teste la conjecture de Golbach en tentant de décomposer n pair (>2) en 2 nb premiers"""
 
-    # verification
-    if n<4 or n%2!=0:
-        raise(ValueError) ("Erreur: n doit être un nombre entier pair > 2")
-
-    # traitement du cas x=2 qui ne marche que pour n=4
-    if n==4:
-        return [2,2]
-
-    # autres cas
     x = 3
     while True:
-
         # calcul de y et test de primalité
         y = n-x
-        if gmpy2.is_prime(y,30):
-            #print(n,[x,y]) # on a trouvé!
-            return (n,[x,y])
+        if gmpy2.is_prime(y,20):
+            return (n,[int(x),int(y)])
 
-        # vérif qu'on a encore la possibilité de trouver une solution
-        if x>y:
-            return [] # echec!
-
-        # On prend le nombre premier suivant x
+        # On prend le nombre premier suivant
         x = gmpy2.next_prime(x)
 
 if __name__ == '__main__':
-    pool=Pool(None)
     a=time.clock()
-    for k in range(repet):
-        result = pool.map(goldbach, [i for i in range(n,n+etape,2)])
-        for l in range(len(result)):
-            print(result[l])
-        n+=10**5
-        p.push("Golbach","Nouvelle étape atteinte !", "10^38 + "+str(k)+"10^6")
+    n=4*10**14
+    nbr_boucle=0
+    with Pool(None) as pool:
+        while 1:
+            nbr_boucle+=1
+            #On va changer la sortie du terminal pour la rédiriger directement dans in fichier
+            #On sauvegarde l'état précedent pour pouvoir le restaurer par la suite
+            stdout_old=sys.stdout
+            sys.stdout = open(out_dir,'a')
+            a=time.process_time()
+            for i in range(step_msg):
+                result = pool.map(goldbach, [i for i in range(n,n+etape,2)])
+                print(*result,sep='\n')
+                n+=etape
+            b=time.process_time()
+            p.push("Golbach","Nouvelle étape atteinte !", str(nbr_boucle)+'*10^'+str(int(log(step_msg*etape)/log(10))+1))
+            sys.stdout.close()
+            # On restaure stdout pour retrouver une sortie terminal
+            sys.stdout=stdout_old
+            print('Etape actuelle :', n)
+            print('Temps nécessaire : ',int((b-a)*10))
 
-    b=time.clock()
-    print('took ',b-a)
