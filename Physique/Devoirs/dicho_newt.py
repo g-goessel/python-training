d01e0211df0c6bae83e16e7316ba1e2264b9bc3f
g-goessel <g_goessel@outlook.com>
2014-02-15 19:23:48 +0100

    DM finished, just need the final ajustements

--- Physique/Devoirs/dicho_newt.py
+++ Physique/Devoirs/dicho_newt.py
@@ -1,20 +1,20 @@
-from scipy import *
+from scipy import misc
 
 def dichotomie(F,a,b,epsilon):
     ite=0
-    if F(a)>F(b): 
-        sign=-1
+    if F(a)<F(b): 
+        sign=1
     else: 
-        sign =1
+        sign =-1
         
-    while abs(b-a) >epsilon:
+    while abs(a-b)>epsilon:
         ite+=1
         c=(a+b)/2
-        if sign*F(c)>0:
+        if F(c)==0: return (c,ite)
+        elif sign*F(c)>0:
             b=c
         else: 
             a=c
-
         print(a,b,c,ite)
             
     return (a,b,ite)
@@ -27,7 +27,7 @@ def newton(F,a,epsilon,DFDT='none'):
         
     while abs(F(a))>epsilon:
         #On verifie si on a trouvé une racine
-        if F(a)==0: break
+        if abs(F(a))<=epsilon: return(a)
         #sinon on fait une boucle supplémentaire
         ite+=1
         a = -F(a)/DFDT +a
@@ -36,11 +36,28 @@ def newton(F,a,epsilon,DFDT='none'):
     return (a,ite)
         
 
-        
-        
+def secante(F,a,b,epsilon):
+    ite=0
+
+    while abs(F(a))>epsilon:
+        #On verifie si on a trouvé une racine
+        if F(a)==0 : return(a,ite)
+        elif F(b)==0 : return(b,ite)
+        #sinon on fait une boucle supplémentaire
+
+        #ici a = x_{n+1}
+        #     b = x_n
+        #     c = x_{n-1}
+        ite+=1
+        c = b
+        b = a
+        a=b - F(b)*(b-c)/(F(b)-F(c))
+    return(a,ite)
 
 def func(x):
-    return -x+3
+    return (x-123)**2-8
     
-print(dichotomie(func,-30,12.30,10**(-2)))
+print(dichotomie(func,120,134.30,10**(-5)))
+print(newton(func,1120,10**(-5)))
+print(secante(func,1120,1234.30,10**(-5)))
     
