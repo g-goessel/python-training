5f4c39e77468daad233d10680cadeba6605eca28
g-goessel <g_goessel@outlook.com>
2014-02-15 18:11:13 +0100

    fixed the dichotomie algorithm and Newton

--- Physique/Devoirs/dicho_newt.py
+++ Physique/Devoirs/dicho_newt.py
@@ -1,4 +1,4 @@
-from scipy import misc
+from scipy import *
 
 def dichotomie(F,a,b,epsilon):
     ite=0
@@ -8,33 +8,39 @@ def dichotomie(F,a,b,epsilon):
         sign =1
         
     while abs(b-a) >epsilon:
-        c=(a-b)/2
+        ite+=1
+        c=(a+b)/2
         if sign*F(c)>0:
-            a=c
-        else: 
             b=c
+        else: 
+            a=c
+
+        print(a,b,c,ite)
             
     return (a,b,ite)
     
     
-def newton(F,a,epsilon,DFDT=0):
+def newton(F,a,epsilon,DFDT='none'):
     ite=0
-    if DFDT==0:
+    if DFDT=='none':
         DFDT=misc.derivative(F,a,10**(-5))
         
-    while F(a)>epsilon and epsilon>F(a):
+    while abs(F(a))>epsilon:
+        #On verifie si on a trouvé une racine
+        if F(a)==0: break
+        #sinon on fait une boucle supplémentaire
         ite+=1
-        a = +F(a)/DFDT +a
+        a = -F(a)/DFDT +a
         DFDT=misc.derivative(F,a,10**(-5))
         
-    return a,ite
+    return (a,ite)
         
 
         
         
 
 def func(x):
-    return x**2
+    return -x+3
     
-print(newton(func,-3,0.1))
+print(dichotomie(func,-30,12.30,10**(-2)))
     
