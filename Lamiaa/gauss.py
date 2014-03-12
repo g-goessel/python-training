ee0685603cb1daf9ee3e1f8b6deee1a70b81913a
g.goessel <g_goessel@outlook.com>
2014-03-12 21:42:02 +0100

    Added Lamiaa's gauss algorithm first try

---
+++ Lamiaa/gauss.py
@@ -0,0 +1,11 @@
+from numpy import *
+from scipy import *
+
+liste=array([[1,2,3,4],
+	        [2,3,4,5],
+	        [3,4,8,6]])
+
+print(liste[2,2])
+listee=array([2,7,9])
+for k in range(len(liste)):
+	for i in range(k+)
